8f6f6d9123036bb990ea84566544f5b62cc4a817
Abel Santillan Rodriguez <abelsantillanrdz@gmail.com>
2026-04-25 21:37:39 -0600

    feat: Expand exports in phoson_agent module to include additional event classes

--- phoson_agent/__init__.py
+++ phoson_agent/__init__.py
@@ -1,10 +1,33 @@
 from .agent import AgentEngine
-from .models import RunStep, AgentTool, ToolHandler, AgentRunResult
+from .models import (
+    RunStep,
+    AgentTool,
+    AgentEvent,
+    ToolHandler,
+    AgentDoneEvent,
+    AgentRunResult,
+    AgentErrorEvent,
+    AgentStartEvent,
+    AgentTokenEvent,
+    AgentStepDoneEvent,
+    AgentToolDoneEvent,
+    AgentReasoningEvent,
+    AgentToolStartEvent,
+)
 
 __all__ = [
     "AgentEngine",
     "AgentTool",
     "ToolHandler",
+    "AgentEvent",
+    "AgentStartEvent",
+    "AgentTokenEvent",
+    "AgentReasoningEvent",
+    "AgentToolStartEvent",
+    "AgentToolDoneEvent",
+    "AgentStepDoneEvent",
+    "AgentDoneEvent",
+    "AgentErrorEvent",
     "RunStep",
     "AgentRunResult",
 ]
